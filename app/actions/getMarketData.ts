7e9a5179daf8fd73a3e0f6055563a4e1bbef9c23
Daniel Santamaria <santamaria.daniel316@gmail.com>
2025-01-17 16:53:26 -0800

    Update config for serverless python

--- app/actions/getMarketData.ts
+++ app/actions/getMarketData.ts
@@ -1,12 +1,15 @@
 "use server";
 import { PredictionData } from "../page";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
-const RENDER_URL = process.env.NEXT_PUBLIC_RENDER_URL || "";
-
 export async function getMarketData() {
   try {
-    const url = new URL("/api/py/getMarketData", API_URL).toString();
+    const url =
+      process.env.NODE_ENV === "development"
+        ? new URL(
+            "/api/py/getMarketData",
+            process.env.NEXT_PUBLIC_API_URL
+          ).toString()
+        : "/api/py/getMarketData";
 
     const response = await fetch(url, {
       method: "GET",
@@ -25,30 +28,6 @@ export async function getMarketData() {
     return data;
   } catch (error) {
     console.error("Market Data Error:", error);
-    throw error;
-  }
-}
-
-export async function getPrediction(financialData: PredictionData) {
-  try {
-    const response = await fetch(RENDER_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(financialData),
-    });
-
-    if (!response.ok) {
-      console.error(
-        `Prediction API Error: ${response.status} - ${response.statusText}`
-      );
-      throw new Error(`HTTP error with status: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Prediction Error:", error);
     throw error;
   }
 }
